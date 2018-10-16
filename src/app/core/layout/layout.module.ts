9b992a510d8006384d581f73934e28c05bbfd662
sercan <sercanyemen@gmail.com>
2018-10-16 23:35:01 +0300

    (Thin Layout) Added the 'thin' layout - same as the 'dense' but cannot rollover to reveal the full menu

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 import { ClassicLayoutModule } from 'app/core/layout/classic/classic.module';
 import { CompactLayoutModule } from 'app/core/layout/compact/compact.module';
 import { DenseLayoutModule } from 'app/core/layout/dense/dense.module';
+import { ThinLayoutModule } from 'app/core/layout/thin/thin.module';
 
 @NgModule({
     imports: [
         ClassicLayoutModule,
         CompactLayoutModule,
-        DenseLayoutModule
+        DenseLayoutModule,
+        ThinLayoutModule
     ],
     exports: [
         ClassicLayoutModule,
         CompactLayoutModule,
-        DenseLayoutModule
+        DenseLayoutModule,
+        ThinLayoutModule
     ]
 })
 export class LayoutModule
---
+++ src/app/core/layout/thin/thin.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+
+import { AsmSharedModule } from '@assembly/shared.module';
+import { AsmDrawerModule, AsmNavigationModule, AsmThemeConfiguratorModule } from '@assembly/components';
+
+import { ThinLayoutComponent } from 'app/core/layout/thin/thin.component';
+
+@NgModule({
+    declarations: [
+        ThinLayoutComponent
+    ],
+    imports     : [
+        RouterModule,
+        MatButtonModule,
+        MatIconModule,
+
+        AsmSharedModule,
+        AsmDrawerModule,
+        AsmNavigationModule,
+        AsmThemeConfiguratorModule
+    ],
+    exports     : [
+        ThinLayoutComponent
+    ]
+})
+export class ThinLayoutModule
+{
+}
