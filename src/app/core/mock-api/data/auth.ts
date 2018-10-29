f32e7ecf45eefbed3671347a3f33a23d2ec1cc29
sercan <sercanyemen@gmail.com>
2018-10-29 21:05:40 +0300

    (Mock API) Added navigation data and did some improvements on the Auth data

--- src/app/core/mock-api/data/auth.ts
+++ src/app/core/mock-api/data/auth.ts
@@ -12,13 +12,13 @@ export class MockAuthData
         username: string;
         name: string;
         email: string;
-        token: string
+        accessToken: string
     } = {
-        id      : '1',
-        username: 'watkins',
-        name    : 'Andrew Watkins',
-        email   : 'watkinsandrew@mail.com',
-        token   : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBc3NlbWJseSIsImlhdCI6MTU0MDM4NTAyMCwiZXhwIjoxNjM1MDc5NDIwLCJhdWQiOiIiLCJzdWIiOiIiLCJpZCI6IjEiLCJ1c2VybmFtZSI6IndhdGtpbnMiLCJuYW1lIjoiQW5kcmV3IFdhdGtpbnMiLCJlbWFpbCI6IndhdGtpbnNhbmRyZXdAbWFpbC5jb20ifQ.qAeRZDE3AqdyjYB9GEc7FXvAGY50TZz99-fZ2uSwVeM'
+        id         : '1',
+        username   : 'watkins',
+        name       : 'Andrew Watkins',
+        email      : 'watkinsandrew@mail.com',
+        accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBc3NlbWJseSIsImlhdCI6MTU0MDM4NTAyMCwiZXhwIjoxNjM1MDc5NDIwLCJhdWQiOiIiLCJzdWIiOiIiLCJpZCI6IjEiLCJ1c2VybmFtZSI6IndhdGtpbnMiLCJuYW1lIjoiQW5kcmV3IFdhdGtpbnMiLCJlbWFpbCI6IndhdGtpbnNhbmRyZXdAbWFpbC5jb20ifQ.qAeRZDE3AqdyjYB9GEc7FXvAGY50TZz99-fZ2uSwVeM'
     };
 
     /**
--- src/app/core/mock-api/mock-api.module.ts
+++ src/app/core/mock-api/mock-api.module.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
 import { MockAuthData } from 'app/core/mock-api/data/auth';
+import { MockNavigationData } from 'app/core/mock-api/data/navigation';
 import { MockNotificationsData } from 'app/core/mock-api/data/notifications';
 
 const mockAdapter = new MockAdapter(axios);
@@ -13,15 +14,18 @@ export class MockApiModule
     /**
      * Constructor
      *
-     * @param _mockAuthData
-     * @param _mockNotificationsData
+     * @param {MockAuthData} _mockAuthData
+     * @param {MockNavigationData} _mockNavigationData
+     * @param {MockNotificationsData} _mockNotificationsData
      */
     constructor(
         private _mockAuthData: MockAuthData,
+        private _mockNavigationData: MockNavigationData,
         private _mockNotificationsData: MockNotificationsData
     )
     {
         this._mockAuthData.init(mockAdapter);
+        this._mockNavigationData.init(mockAdapter);
         this._mockNotificationsData.init(mockAdapter);
     }
 }
