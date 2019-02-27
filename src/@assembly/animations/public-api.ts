14234376e056fec00b0a5ddf475a5a3b27d76a7f
sercan <sercanyemen@gmail.com>
2019-02-27 10:58:04 +0300

    Added a 'shake' animation

--- src/@assembly/animations/public-api.ts
+++ src/@assembly/animations/public-api.ts
@@ -1,6 +1,7 @@
 import { expandCollapse } from '@assembly/animations/expand-collapse';
 import { fadeIn } from '@assembly/animations/fade-in';
 import { fadeOut } from '@assembly/animations/fade-out';
+import { shake } from '@assembly/animations/shake';
 import { slideIn } from '@assembly/animations/slide-in';
 import { slideOut } from '@assembly/animations/slide-out';
 import { zoomIn } from '@assembly/animations/zoom-in';
@@ -10,6 +11,7 @@ export const AsmAnimations = [
     expandCollapse,
     fadeIn,
     fadeOut,
+    shake,
     slideIn,
     slideOut,
     zoomIn,
