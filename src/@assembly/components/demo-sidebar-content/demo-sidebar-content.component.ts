19f8818dbfa39fa667ad8ba8cd0ef92da3ce645b
sercan <sercanyemen@gmail.com>
2019-04-22 17:41:40 +0300

    (AsmDemoSidebar) Use AsmVerticalMenu as the navigation

--- src/@assembly/components/demo-sidebar-content/demo-sidebar-content.component.ts
+++ src/@assembly/components/demo-sidebar-content/demo-sidebar-content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AsmVerticalMenuItem } from '@assembly';
 
 @Component({
     selector   : 'asm-demo-sidebar-content',
@@ -7,9 +8,98 @@ import { Component } from '@angular/core';
 })
 export class AsmDemoSidebarContentComponent
 {
+    menuData: AsmVerticalMenuItem[];
 
     constructor()
     {
-    }
+        // Set the defaults
+        this.menuData = [
+            {
+                title   : 'Subheader',
+                subtitle: 'Subheader subtitle',
+                type    : 'subheader',
+                icon    : 'apps'
+            },
+            {
+                title     : 'Menu Item 1',
+                type      : 'basic',
+                icon      : 'dashboards',
+                exactMatch: true
+            },
+            {
+                title   : 'Menu Item 2',
+                subtitle: 'Item subtitle',
+                type    : 'basic',
+                icon    : 'today'
+            },
+            {
+                title: 'Menu Item 3',
+                type : 'basic',
+                icon : 'email',
+                badge: {
+                    title     : '9',
+                    background: '#E91E63',
+                    color     : '#FFFFFF'
+                }
+            },
+            {
+                title: 'Menu Item 4',
+                type : 'basic',
+                icon : 'check_box'
+            },
+            {
+                title: 'Menu Item 5',
+                type : 'basic',
+                icon : 'note'
+            },
+            {
+                title: 'Menu Item 6',
+                type : 'basic',
+                icon : 'account_box'
+            },
+            {
+                title: 'Subheader',
+                type : 'subheader'
+            },
+            {
+                title   : 'Menu Item 1',
+                type    : 'collapsable',
+                icon    : 'lock',
+                children: [
+                    {
+                        title: 'Menu Item Children 1',
+                        type : 'basic'
+                    },
+                    {
+                        title: 'Menu Item Children 2',
+                        type : 'basic'
+                    },
+                    {
+                        title: 'Menu Item Children 3',
+                        type : 'basic'
+                    },
+                    {
+                        title: 'Menu Item Children 4',
+                        type : 'basic'
+                    }
+                ]
+            },
+            {
+                title: 'Menu Item 2',
+                type : 'basic',
+                icon : 'hourglass_empty'
+            },
+            {
+                title: 'Menu Item 3',
+                type : 'basic',
+                icon : 'build'
+            },
+            {
+                title: 'Menu Item 4',
+                type : 'basic',
+                icon : 'person'
+            }
+        ];
 
+    }
 }
--- src/@assembly/components/demo-sidebar-content/demo-sidebar-content.module.ts
+++ src/@assembly/components/demo-sidebar-content/demo-sidebar-content.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material';
+import { AsmVerticalMenuModule } from '@assembly/components/menu/vertical-menu/vertical-menu.module';
 import { AsmDemoSidebarContentComponent } from '@assembly/components/demo-sidebar-content/demo-sidebar-content.component';
 
 @NgModule({
     declarations: [
         AsmDemoSidebarContentComponent
+    ],
+    imports     : [
+        RouterModule.forChild([]),
+        MatIconModule,
+        AsmVerticalMenuModule
     ],
     exports     : [
         AsmDemoSidebarContentComponent
