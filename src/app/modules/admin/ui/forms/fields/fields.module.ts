11491a72ade09f05274f98de8276ccf82e86c975
sercan <sercanyemen@gmail.com>
2019-11-05 20:20:15 +0300

    (ui/forms) Small tweaks

--- src/app/modules/admin/ui/forms/fields/fields.module.ts
+++ src/app/modules/admin/ui/forms/fields/fields.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
 import { AsmCardModule } from '@assembly';
 import { SharedModule } from 'app/shared/shared.module';
 import { FormsFieldsComponent } from 'app/modules/admin/ui/forms/fields/fields.component';
@@ -16,19 +13,16 @@ import { formsFieldsRoutes } from 'app/modules/admin/ui/forms/fields/fields.rout
 
 @NgModule({
     declarations: [
-        FormsFieldsComponent,
+        FormsFieldsComponent
     ],
     imports     : [
         RouterModule.forChild(formsFieldsRoutes),
         MatButtonModule,
-        MatCheckboxModule,
         MatDividerModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule,
         MatMenuModule,
-        MatRadioModule,
-        MatSelectModule,
         AsmCardModule,
         SharedModule
     ]
