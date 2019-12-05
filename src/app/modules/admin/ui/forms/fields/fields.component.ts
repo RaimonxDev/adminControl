55151a8de3b390bca4f0233a07e918e3ad79b424
sercan <sercanyemen@gmail.com>
2019-12-05 20:22:08 +0300

    (ui/forms) Small tweak

--- src/app/modules/admin/ui/forms/fields/fields.component.ts
+++ src/app/modules/admin/ui/forms/fields/fields.component.ts
@@ -1,22 +1,17 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-    selector       : 'forms-fields',
-    templateUrl    : './fields.component.html',
-    styleUrls      : ['./fields.component.scss'],
-    encapsulation  : ViewEncapsulation.None
+    selector     : 'forms-fields',
+    templateUrl  : './fields.component.html',
+    styleUrls    : ['./fields.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class FormsFieldsComponent
 {
-    formFieldAppearance: 'standard' | 'fill' | 'outline';
-
     /**
      * Constructor
      */
     constructor()
     {
-        // Set the default
-        this.formFieldAppearance = 'fill';
     }
-
 }
