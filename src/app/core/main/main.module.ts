ecd80c2724bf7b0d112c70d081c6e7a7b41fcbc2
sercan <sercanyemen@gmail.com>
2019-12-22 17:08:27 +0300

    Initial version of the configuration drawer

--- src/app/core/main/main.module.ts
+++ src/app/core/main/main.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AsmDrawerModule } from '@assembly';
 import { SharedModule } from 'app/shared/shared.module';
 import { MainComponent } from 'app/core/main/main.component';
@@ -15,6 +16,7 @@ import { LayoutsModule } from 'app/core/main/layouts/layouts.module';
         MatButtonModule,
         MatIconModule,
         MatRadioModule,
+        MatSlideToggleModule,
         AsmDrawerModule,
         LayoutsModule,
         SharedModule
