e6f5cd006f1c542cdb71310d96050ad00d63bb9f
sercan <sercanyemen@gmail.com>
2020-02-07 17:03:31 +0300

    Updated Tailwind to 1.2.0

--- src/tailwind/config.js
+++ src/tailwind/config.js
@@ -184,6 +184,22 @@ module.exports = {
             },
 
             fontFamily: {
+                sans: [
+                    'Inter',
+                    'system-ui',
+                    '-apple-system',
+                    'BlinkMacSystemFont',
+                    '"Segoe UI"',
+                    'Roboto',
+                    '"Helvetica Neue"',
+                    'Arial',
+                    '"Noto Sans"',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
                 mono: ['"IBM Plex Mono"', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace']
             },
 
