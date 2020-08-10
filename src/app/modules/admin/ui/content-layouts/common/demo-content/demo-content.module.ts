86de6236c3e36a70e8664789be76c96faae9124f
sercan <sercanyemen@gmail.com>
2020-08-10 15:43:32 +0300

    (@treo) Updated and tweaked ".rich-text" helper styles

--- src/app/modules/admin/ui/content-layouts/common/demo-content/demo-content.module.ts
+++ src/app/modules/admin/ui/content-layouts/common/demo-content/demo-content.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { DemoContentComponent } from 'app/modules/admin/ui/content-layouts/common/demo-content/demo-content.component';
 
 @NgModule({
     declarations: [
         DemoContentComponent
+    ],
+    imports     : [
+        RouterModule.forChild([])
     ],
     exports     : [
         DemoContentComponent
