7d11884ca2f6d2c4770ed545dfb9eb5d59ac151d
sercan <sercanyemen@gmail.com>
2020-10-05 17:32:08 +0300

    Better Tailwind integration with Treo and Angular

--- src/@treo/tailwind/plugins/treo.js
+++ src/@treo/tailwind/plugins/treo.js
@@ -5,13 +5,13 @@ const _ = require('lodash');
 // -----------------------------------------------------------------------------------------------------
 // @ Global variables for filling the config object from the plugin
 // -----------------------------------------------------------------------------------------------------
-const _allContrasts = {
+const contrasts = {
     current    : null,
     transparent: null,
     white      : '#000000',
     black      : '#FFFFFF'
 };
-const _materialPalettes = {};
+const materialPalettes = {};
 
 // -----------------------------------------------------------------------------------------------------
 // @ TREO TailwindCSS Main Plugin
@@ -27,10 +27,10 @@ module.exports = plugin(({addVariant, variants, theme, e, postcss}) =>
             {
                 // If the contrast map for this color is provided by the user,
                 // use it. Otherwise create it from palette itself.
-                let contrasts = theme('treo.contrasts.' + key);
-                if ( !contrasts )
+                let colorContrasts = theme('treo.contrasts.' + key);
+                if ( !colorContrasts )
                 {
-                    contrasts = {};
+                    colorContrasts = {};
 
                     // Sort the palette colors from lighter to darker to
                     // get the lightest and darkest colors as Color so
@@ -45,16 +45,16 @@ module.exports = plugin(({addVariant, variants, theme, e, postcss}) =>
                     // contrasts.
                     Object.entries(colors).forEach(([hue, color]) =>
                     {
-                        contrasts[hue] = colorLib(color).contrast(lightestColor) > colorLib(color).contrast(darkestColor) ? lightestColor.hex() : darkestColor.hex();
+                        colorContrasts[hue] = colorLib(color).contrast(lightestColor) > colorLib(color).contrast(darkestColor) ? lightestColor.hex() : darkestColor.hex();
                     });
                 }
 
                 // Merge colors and contrasts in _materialPalettes
-                _materialPalettes[key] = colors;
-                _materialPalettes[key].contrast = contrasts;
+                materialPalettes[key] = colors;
+                materialPalettes[key].contrast = colorContrasts;
 
                 // Merge contrasts in _allContrasts
-                _allContrasts[key] = contrasts;
+                contrasts[key] = colorContrasts;
             });
 
 
@@ -73,7 +73,7 @@ module.exports = plugin(({addVariant, variants, theme, e, postcss}) =>
             console.dir(theme('treo._materialPalettes'));
 
             // -----------------------------------------------------------------------------------------------------
-            // @ Generate SCSS map of Material palettes and append it
+            // @ Generate Angular Material compatible palettes map
             // -----------------------------------------------------------------------------------------------------
             // Prepare the map
             let map = '';
@@ -119,16 +119,61 @@ module.exports = plugin(({addVariant, variants, theme, e, postcss}) =>
                     value: `(\n ${map} ) !default`
                 })
             );
+
+            // -----------------------------------------------------------------------------------------------------
+            // @ Generate Angular Material theme generator code
+            // -----------------------------------------------------------------------------------------------------
+            // Prepare the string
+            let themeGenCode = '';
+
+            // Iterate through themes
+            Object.entries(theme('treo.themes'))
+                .forEach(([name, theme]) =>
+                {
+                    // Generate modified foreground colors map for this theme
+                    let foregroundMap = '';
+                    Object.entries(theme('treo.material.colors.foreground.' + theme.scheme)).forEach(([name, color]) =>
+                    {
+                        foregroundMap = `${foregroundMap} ${name}:${color},\n`;
+                    });
+
+                    // Generate modified background colors map for this theme
+                    let backgroundMap = '';
+                    Object.entries(theme('treo.material.colors.background.' + theme.scheme)).forEach(([name, color]) =>
+                    {
+                        backgroundMap = `${backgroundMap} ${name}:${color},\n`;
+                    });
+
+                    // Generate the code for the theme
+                    themeGenCode = `${themeGenCode}
+                        
+                        $angular-material-theme-for-${name}: ${theme.scheme === 'light' ? 'mat-light-theme' : 'mat-dark-theme'}
+                        (
+                            (
+                                color: (
+                                    primary: mat-palette(map-get($treo-material-palettes, ${theme.scheme.primary[0]}, ${theme.scheme.primary[1]}, 100, 700, ${theme.scheme.primary[1]})),
+                                    accent: mat-palette(map-get($treo-material-palettes, ${theme.scheme.accent[0]}, ${theme.scheme.accent[1]}, 100, 700, ${theme.scheme.accent[1]})),
+                                    warn: mat-palette(map-get($treo-material-palettes, ${theme.scheme.warn[0]}, ${theme.scheme.warn[1]}, 100, 700, ${theme.scheme.warn[1]}))
+                                )
+                            )
+                        );
+                        
+                        $foreground-colors-for-${name}: (
+                            ${foregroundMap}
+                        );
+                        
+                        $background-colors-for-${name}: (
+                            ${backgroundMap}
+                        );
+                        
+                        .${name} {
+                            
+                        }
+                    `;
+                });
+
         });
 
-
-        // Go through all available colors from 'theme.colors' and
-
-        // const ratio1 = wcagContrast.hex('rgba(0,0,0,0.12)', theme('colors.red.50'));
-        // const ratio2 = wcagContrast.hex(theme('colors.red.500'), theme('colors.red.900'));
-
-        // console.log(ratio1);
-        // console.log(ratio2);
 
         /*addVariant('treo-angular-material-themes', ({container}) =>
         {
@@ -158,8 +203,82 @@ module.exports = plugin(({addVariant, variants, theme, e, postcss}) =>
             treo    : {
                 contrasts: {},
                 themes   : {},
-                _allContrasts,
-                _materialPalettes
+                material : {
+                    colors: {
+                        foreground: {
+                            light: {
+                                'base'             : '#000000',
+                                'divider'          : '#E2E8F0',
+                                'dividers'         : '#E2E8F0',
+                                'disabled'         : '#97A6BA',
+                                'disabled-button'  : '#97A6BA',
+                                'disabled-text'    : '#97A6BA',
+                                'elevation'        : '#000000',
+                                'hint-text'        : '#97A6BA',
+                                'secondary-text'   : '#64748B',
+                                'icon'             : '#64748B',
+                                'icons'            : '#64748B',
+                                'text'             : '#27303F',
+                                'slider-min'       : '#27303F',
+                                'slider-off'       : '#CFD8E3',
+                                'slider-off-active': '#97A6BA'
+                            },
+                            dark : {
+                                'base'             : '#FFFFFF',
+                                'divider'          : 'rgba(#F1F5F9, 0.12)',
+                                'dividers'         : 'rgba(#F1F5F9, 0.12)',
+                                'disabled'         : '#475569',
+                                'disabled-button'  : '#27303F',
+                                'disabled-text'    : '#475569',
+                                'elevation'        : '#000000',
+                                'hint-text'        : '#64748B',
+                                'secondary-text'   : '#97A6BA',
+                                'icon'             : '#F1F5F9',
+                                'icons'            : '#F1F5F9',
+                                'text'             : '#FFFFFF',
+                                'slider-min'       : '#FFFFFF',
+                                'slider-off'       : '#64748B',
+                                'slider-off-active': '#97A6BA'
+                            }
+                        },
+                        background: {
+                            light: {
+                                'status-bar'              : '#CFD8E3',
+                                'app-bar'                 : '#FFFFFF',
+                                'background'              : '#F1F5F9',
+                                'hover'                   : 'rgba(#97A6BA, 0.12)',
+                                'card'                    : '#FFFFFF',
+                                'dialog'                  : '#FFFFFF',
+                                'disabled-button'         : 'rgba(#97A6BA, 0.38)',
+                                'raised-button'           : '#FFFFFF',
+                                'focused-button'          : '#64748B',
+                                'selected-button'         : '#E2E8F0',
+                                'selected-disabled-button': '#E2E8F0',
+                                'disabled-button-toggle'  : '#CFD8E3',
+                                'unselected-chip'         : '#E2E8F0',
+                                'disabled-list-option'    : '#CFD8E3',
+                                'tooltip'                 : '#27303F'
+                            },
+                            dark : {
+                                'status-bar'              : '#1A202E',
+                                'app-bar'                 : '#1A202E',
+                                'background'              : '#1A202E',
+                                'hover'                   : 'rgba(255, 255, 255, 0.05)',
+                                'card'                    : '#27303F',
+                                'dialog'                  : '#27303F',
+                                'disabled-button'         : 'rgba(#1A202E, 0.38)',
+                                'raised-button'           : '#1A202E',
+                                'focused-button'          : '#E2E8F0',
+                                'selected-button'         : 'rgba(255, 255, 255, 0.05)',
+                                'selected-disabled-button': '#27303F',
+                                'disabled-button-toggle'  : '#1A202E',
+                                'unselected-chip'         : '#475569',
+                                'disabled-list-option'    : '#E2E8F0',
+                                'tooltip'                 : '#64748B'
+                            }
+                        }
+                    }
+                }
             },
             fontSize: {
                 'xs'  : '0.625rem',
@@ -429,7 +548,6 @@ module.exports = plugin(({addVariant, variants, theme, e, postcss}) =>
             transitionDuration      : [],
             transitionDelay         : []
         },
-        // Core plugins
         corePlugins: {
             animation         : false,
             appearance        : false,
