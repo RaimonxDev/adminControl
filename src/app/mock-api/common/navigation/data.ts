d5e831fd1671e00964005648d4df99256396d48c
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-04-19 09:37:05 -0400

    productos nav

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -66,6 +66,28 @@ export const futuristicNavigation: TreoNavigationItem[] = [
         type : 'basic',
         icon : 'heroicons_outline:user-group',
         link : '/customers'
+    },
+   {
+        id   : 'productos',
+        title: 'Productos',
+        type : 'collapsable',
+        icon : 'heroicons_outline:collection',
+        children: [
+        {
+            id   : 'crear-producto',
+            title: 'Crear Nuevo Producto',
+            type : 'basic',
+            icon : 'heroicons_outline:document-add',
+            link : 'productos/create'
+        },
+        {
+            id   : 'ver-productos',
+            title: 'Todos los productos',
+            type : 'basic',
+            icon : 'heroicons_outline:clipboard-list',
+            link : 'orders/shipped'
+        }
+        ]
     }
 ];
 export const horizontalNavigation: TreoNavigationItem[] = [
