3d6f3a81b733f028411960fb0f6e2b74ab068d73
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-05-22 09:36:41 -0400

    add param duration

--- src/app/shared/services/notifier.service.ts
+++ src/app/shared/services/notifier.service.ts
@@ -3,31 +3,29 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { AlertComponent } from '../alert/alert.component';
 import { TreoAlertType } from '../../../@treo/components/alert/alert.types';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotifierService {
-
-  constructor(private snackBar:MatSnackBar) {
-
-  }
-  showNotification (titleMessage: string, displayMessage: string , typeAlert: TreoAlertType, buttonText: string | null, ) {
-
+  constructor(private snackBar: MatSnackBar) {}
+  showNotification(
+    titleMessage: string,
+    displayMessage: string,
+    typeAlert: TreoAlertType,
+    buttonText: string | null,
+    duration: number = 1500
+  ) {
     this.snackBar.openFromComponent(AlertComponent, {
       data: {
-       message: displayMessage,
-       buttonText,
-       typeAlert,
-       titleMessage
+        message: displayMessage,
+        buttonText,
+        typeAlert,
+        titleMessage,
       },
-      verticalPosition:'top',
+      verticalPosition: 'top',
       horizontalPosition: 'center',
-      duration: 1500,
-      panelClass:'transparent'
-    })
-
-
+      duration: duration,
+      panelClass: 'transparent',
+    });
   }
 }
