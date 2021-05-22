6398308743c7fce867622cb2d9c03489f0d53176
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-05-22 09:37:17 -0400

    create class ErrorResponse new features

--- src/app/shared/utils/Validators/httpErrorHandler.ts
+++ src/app/shared/utils/Validators/httpErrorHandler.ts
@@ -1,21 +1,54 @@
-import { HttpErrorResponse} from '@angular/common/http'
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-import { throwError } from 'rxjs'
+import { throwError } from 'rxjs';
+import { NotifierService } from '../../services/notifier.service';
 
-export function HandleHttpResponseError(error: HttpErrorResponse, message: string) {
+export function HandleHttpResponseError(
+  error: HttpErrorResponse,
+  message?: string
+) {
   switch (error.status) {
-
     case 401:
-     return throwError(`Usted No tiene persimos para hacer esta operacion ${error.status}`)
-
+      return throwError(
+        `Usted No tiene persimos para hacer esta operacion ${error.status}`
+      );
     case 403:
-      return throwError(`Acceso Denegado ${error.status}`)
+      return throwError(`Acceso Denegado ${error.status}`);
 
     case 0:
-      return throwError(message)
+      return throwError(message);
 
     default:
       break;
   }
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorResponseHttp {
+  constructor(private notify: NotifierService) {}
 
+  handleError(error: HttpErrorResponse, message?: string, duration?: number) {
+    switch (error.status) {
+      case 401:
+        return throwError(
+          `Usted No tiene persimos para hacer esta operacion ${error.status}`
+        );
+      case 403:
+        this.notify.showNotification(
+          'Error',
+          'No tiene permisos',
+          'error',
+          null,
+          duration
+        );
+        return throwError(`Acceso Denegado ${error.status}`);
+      case 0:
+        return throwError(message);
+
+      default:
+        break;
+    }
+  }
 }
